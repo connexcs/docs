f1ee3ccbb49e8b895d1bde93a15891eaeee3bdca
Jonathan Hulme <jon@digipigeon.com>
2016-10-05 23:10:08 +0100

    Update extra.js

--- extra.js
+++ extra.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-	$("#rtd-search-form").attr('action', 'https://' + window.location.hostname + '/en/latest/search.html')
+    var target = document.getElementById('rtd-search-form');
+    var config = {attributes: true, childList: true};
+
+    var observer = new MutationObserver(function(mutations) {
+      // if it isn't disconnected it'll loop infinitely because the observed element is modified
+      observer.disconnect();
+      var form = $('#rtd-search-form');
+      form.empty();
+      form.attr('action', 'https://' + window.location.hostname + '/en/latest/search.html');
+      $('<input>').attr({
+        type: "text",
+        name: "q",
+        placeholder: "Search docs"
+      }).appendTo(form);
+    });
+
+    if (window.location.origin.indexOf('readthedocs') > -1) {
+      observer.observe(target, config);
+    }
 });
