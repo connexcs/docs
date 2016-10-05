cf7597f1b3233a92929f3946487b5a72eaae4c62
Jonathan Hulme <jon@digipigeon.com>
2016-10-05 22:58:03 +0100

    Create extra.js

---
+++ extra.js
@@ -0,0 +1,21 @@
+function fixSearch() {
+	var target = document.getElementById('rtd-search-form');
+	var config = {attributes: true, childList: true};
+	
+	var observer = new MutationObserver(function(mutations) {
+	// if it isn't disconnected it'll loop infinitely because the observed element is modified
+	observer.disconnect();
+	var form = $('#rtd-search-form');
+	form.empty();
+	form.attr('action', 'https://' + window.location.hostname + '/en/' + determineSelectedBranch() + '/search.html');
+	$('<input>').attr({
+		type: "text",
+		name: "q",
+		placeholder: "Search docs"
+		}).appendTo(form);
+	});
+	
+	if (window.location.origin.indexOf('readthedocs') > -1) {
+		observer.observe(target, config);
+	}
+}
