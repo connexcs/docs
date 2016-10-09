bb064a13b7336418013c4baf690a7cea519b7538
Jonathan Hulme <jon@digipigeon.com>
2016-10-09 11:26:59 +0100

    Add Online Chat

--- extra.js
+++ extra.js
@@ -19,3 +19,13 @@ $(document).ready(function () {
       observer.observe(target, config);
     }
 });
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/559bcf8dfceed3253707eff4/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
