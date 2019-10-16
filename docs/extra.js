9058827bec6b990d60f5078de88142101a0c78db
Jonathan Hulme <jon@digipigeon.com>
2019-10-16 09:26:10 +0000

    Fix Javascript

--- docs/extra.js
+++ docs/extra.js
@@ -1,28 +1,3 @@
-$(document).ready(function () {
-    var target = document.getElementById('rtd-search-form');
-    var config = {attributes: true, childList: true};
-    $('#rtd-search-form > input').on('keypress', function(){
-      $('#rtd-search-form').attr('action', 'https://' + window.location.hostname + '/en/latest/search.html');
-    });
-    
-    var observer = new MutationObserver(function(mutations) {
-      // if it isn't disconnected it'll loop infinitely because the observed element is modified
-      observer.disconnect();
-      var form = $('#rtd-search-form');
-      form.empty();
-      form.attr('action', 'https://' + window.location.hostname + '/en/latest/search.html');
-      $('<input>').attr({
-        type: "text",
-        name: "q",
-        placeholder: "Search docs"
-      }).appendTo(form);
-    });
-
-    if (window.location.origin.indexOf('readthedocs') > -1) {
-      observer.observe(target, config);
-    }
-});
-
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
