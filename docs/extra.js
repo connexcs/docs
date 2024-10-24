afc1cc99d8e13148e43fdd6330c8763e4073caf8
Ravi kumar <86847625+ravikumar1002@users.noreply.github.com>
2024-10-24 12:29:02 +0000

    added: document subscribe to load extra js code

--- docs/extra.js
+++ docs/extra.js
@@ -1,15 +1,6 @@
-window.$crisp=[];
-window.CRISP_WEBSITE_ID="fdc64bee-6a11-43fc-b928-6829623c9eaa";
-(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
-
-/*
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/559bcf8dfceed3253707eff4/1e75hqurj';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-*/
+document$.subscribe(() => {
+    console.log("Initialize third-party libraries here")
+    window.$crisp=[];
+    window.CRISP_WEBSITE_ID="fdc64bee-6a11-43fc-b928-6829623c9eaa";
+    (function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+})
